dce10176b1a1d4136bb85158b4da7fa019095e53
Robert H Grayson II <bobbygrayson@gmail.com>
2015-03-13 13:40:17 -0400

    add ocr model

--- app/model.rb
+++ app/model.rb
@@ -15,3 +15,17 @@ class User
 end
 
 DataMapper.finalize.auto_upgrade!
+
+class Ocr
+  def init
+    @engine = Tesseract::Engine.new {|engine|
+                engine.language = :lol # arbitrary
+                engine.page_segmentation_mode = 4
+                engine.whitelist = [*'a'..'z', *'A'..'Z', *0..9].join # thanks ruby, made this easy
+              }
+  end
+
+  def get_text(im)
+    @engine.text_for(im)
+  end
+end
