77a2b97ac959bc1cd7cd4dacbd5b8447f4f1f3b4
Robert H Grayson II <bobbygrayson@gmail.com>
2015-03-14 19:44:26 -0400

    integrate tesseract engine to class

--- app/spec/integration_spec.rb
+++ app/spec/integration_spec.rb
@@ -2,9 +2,14 @@ require './ocr_engine.rb'
 
 describe OcrEngine do
   let(:engine) { OcrEngine.new }
+
   describe "#new" do
     it 'creates a new instantiation of the OcrEngine class' do
       expect(engine).to be_an_instance_of OcrEngine
+    end
+    
+    it "creates an engine upon instantiation" do
+      expect(engine.ocr_engine).to be_an_instance_of Tesseract::Engine
     end
   end
 end
