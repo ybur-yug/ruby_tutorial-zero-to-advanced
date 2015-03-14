a1f8736ab76df0eff4cbd39200d75172b73a4806
Robert H Grayson II <bobbygrayson@gmail.com>
2015-03-14 19:33:21 -0400

    initial passing test

---
+++ app/ocr_engine.rb
@@ -0,0 +1,3 @@
+class OcrEngine
+end
+
---
+++ app/spec/integration_spec.rb
@@ -0,0 +1,10 @@
+require './ocr_engine.rb'
+
+describe OcrEngine do
+  let(:engine) { OcrEngine.new }
+  describe "#new" do
+    it 'creates a new instantiation of the OcrEngine class' do
+      expect(engine).to be_an_instance_of OcrEngine
+    end
+  end
+end
