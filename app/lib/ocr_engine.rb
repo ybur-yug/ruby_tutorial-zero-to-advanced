993d72d2b79a1a41ad52c87133669a7908012248
Robert H Grayson II <bobbygrayson@gmail.com>
2015-03-15 18:34:59 -0400

    grab dank memes

--- app/lib/ocr_engine.rb
+++ app/lib/ocr_engine.rb
@@ -1,4 +1,6 @@
 require 'tesseract-ocr'
+require 'open-uri'
+require 'mechanize'
 require 'pry'
 
 module OcrEngine
@@ -8,6 +10,7 @@ module OcrEngine
         e.language = :eng
         e.blacklist = '|'
       }
+      @scraper = Scraper.new
     end
 
     def ocr_engine
@@ -17,7 +20,38 @@ module OcrEngine
     def text_for(im)
       @ocr_engine.text_for(im).strip! 
     end
+
+    def get_memes
+      @scraper.image_links('http://www.reddit.com/r/memes').map { |m|
+        binding.pry 
+      }
+    end
+  end
+
+  class Scraper
+    def initialize
+      @browser = Mechanize.new { |b| 
+        b.user_agent_alias = 'Mac Safari'
+      }
+    end
+    
+    def browser 
+      @browser
+    end
+
+    def get(url)
+      @browser.get(url)
+    end
+
+    def image_links(url)
+      get(url).links.map { |l| l.uri.to_s if check_link(l.uri.to_s) }.compact!
+    end
+
+    private
+
+    def check_link(link)
+      link.include? 'png'
+    end
   end
 end
-
 
