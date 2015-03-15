2c10353a44fb11df818727c1141f70e2831fb2f2
Robert H Grayson II <bobbygrayson@gmail.com>
2015-03-14 20:27:07 -0400

    update to get text

--- app/spec/integration_spec.rb
+++ app/spec/integration_spec.rb
@@ -1,7 +1,9 @@
 require './ocr_engine.rb'
+require 'pry'
 
 describe OcrEngine do
   let(:engine) { OcrEngine.new }
+  let(:tess_engine) { OcrEngine.new.ocr_engine }
 
   describe "#new" do
     it 'creates a new instantiation of the OcrEngine class' do
@@ -10,6 +12,15 @@ describe OcrEngine do
     
     it "creates an engine upon instantiation" do
       expect(engine.ocr_engine).to be_an_instance_of Tesseract::Engine
+    end
+  end
+
+  describe "when processing images" do
+    it "returns text" do
+      File.open("#{Dir.pwd}/rb.png") do |file|
+        result = tess_engine.text_for(file.read)
+        expect(result).to be_an_instance_of String
+      end
     end
   end
 end
---
+++ ocr_engine.rb
@@ -0,0 +1,11 @@
+require 'tesseract-ocr'
+
+class OcrEngine
+  def initialize
+    @ocr_engine = Tesseract::Engine.new
+  end
+
+  def ocr_engine
+    @ocr_engine
+  end
+end
